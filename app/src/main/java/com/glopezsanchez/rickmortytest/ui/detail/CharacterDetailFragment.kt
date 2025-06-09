cdfa1e32775a94cfc291bca38cff9a084a411e8f
Gustavo Lopez <g.lopez.sanchez@accenture.com>
2025-06-09 04:27:37 -0700

    setup ui for detail screen

--- app/src/main/java/com/glopezsanchez/rickmortytest/ui/detail/CharacterDetailFragment.kt
+++ app/src/main/java/com/glopezsanchez/rickmortytest/ui/detail/CharacterDetailFragment.kt
@@ -1,13 +1,23 @@
 package com.glopezsanchez.rickmortytest.ui.detail
 
 import android.os.Bundle
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import coil3.load
+import coil3.request.transformations
+import coil3.transform.CircleCropTransformation
+import com.glopezsanchez.rickmortytest.R
 import com.glopezsanchez.rickmortytest.databinding.FragmentCharacterDetailBinding
 import com.glopezsanchez.rickmortytest.ui.MainViewModel
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class CharacterDetailFragment : Fragment() {
 
@@ -23,8 +33,35 @@ class CharacterDetailFragment : Fragment() {
         return binding.root
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.toolbar.setNavigationIcon(R.drawable.arrow_left)
+        binding.toolbar.setNavigationOnClickListener { requireActivity().supportFragmentManager.popBackStack() }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.detailUiState.collectLatest { state ->
+                state.character?.apply {
+                    binding.image.load(this.picture) {
+                        transformations(
+                            CircleCropTransformation()
+                        )
+                    }
+                    binding.toolbar.title = this.name
+                }
+
+                state.content?.apply {
+                    binding.infoText.text =
+                        Html.fromHtml(state.content, Html.FROM_HTML_MODE_LEGACY)
+                }
+
+                binding.loadingContainer.isVisible = state.isLoading
+
+                if(state.errorMessage != null) {
+                    Toast.makeText(requireContext(), state.errorMessage, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
